ad73ff9926854e5e77eb985d4f57901bf2190eb8
Erhan Aslan <axamedia2@gmail.com>
2019-08-10 23:57:11 +0300

    v1.0.1

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const path = require('path')
+var app = require('express')()
+var http = require('http').createServer(app)
+var port = process.env.PORT || 3000
+var io = require('socket.io')(http)
+
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'))
+    res.render('main')
 })
 
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/signup.html'))
+app.get('/createProfile', (req, res) => {
+    res.render('createProfile')
 })
 
+app.get('/login', (req, res) => {
+    res.render('login')
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/messaging', (req, res) => {
+    res.render('messaging')
+})
+
+io.on('connection', function (socket) {
+    console.log('a user connected')
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg)
+    })
+    socket.on('disconnect', function () {
+        console.log('a user disconnected')
+    })
+})
+
+http.listen(port, () => console.log(`Example app listening on port ${port}!`))
